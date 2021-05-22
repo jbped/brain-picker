a5960243b7f115b7285efa75fee5771872ce9153
jbped <pedigojacob@gmail.com>
2021-05-22 17:08:36 -0600

    Added localContent and created highscore page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // Link JS to HTML Elements
 var timerDisplayEl = document.querySelector('#timer-display');
+var timerDivEl = document.querySelector("#timer");
 var mainTopEl = document.querySelector('#main-top');
 var mainTopTitle = document.querySelector('#main-top-title')
 var mainCenterEl = document.querySelector('#main-center');
@@ -8,8 +9,7 @@ var startBtnEl = document.querySelector('#start-btn');
 var flavorTextEl = document.querySelector('#flavor-text');
 var answerFeedbackEl = document.querySelector("#answer-feedback");
 var formDivEl = document.querySelector("#form-handler");
-// var initialsInputEl = document.getElementById("initials").value.toUpperCase;
-// var submitFormEl = document.getElementById("sub-score");
+var viewHighscoresEl = document.querySelector("#highscore");
 
 // JS Created Elements
 var btnHolder = document.querySelector(".button-holder");
@@ -24,10 +24,9 @@ currentQuestion = 0;
 // Start Timer
 var quizTimer;
 
-// highscore array
-var highscores = [];
+var highscores =  [];
 
-
+var highscoreObj = {};
 
 // Questions and Answers Array
 var questionArr = [ 
@@ -67,12 +66,6 @@ var questionArr = [
     }
 ];
 
-// Clears content and buttons to prepare page for quiz layout
-var quizPrep = function() {
-    flavorTextEl.className = "hidden";
-    startBtnEl.remove();
-}
-
 // Create div for quit and next buttons
 var navBtnDiv = function () {
     var navBtnDiv = document.createElement("div");
@@ -84,13 +77,13 @@ var navBtnDiv = function () {
 // Create quit button (quit quiz)
 var quitBtn = function() {
     var createQuitBtn = document.createElement('button');
-    createQuitBtn.textContent = "Quit"
+    createQuitBtn.textContent = "Quit";
     createQuitBtn.setAttribute("id","quit-btn");
     navBtnDivEl = document.getElementById("nav-btn-div");
     createQuitBtn.addEventListener("click", function(){
        
         if(confirm("Are you sure you would like to quit?")) {
-            window.location.reload()
+            window.location.reload();
         }
     });
     navBtnDivEl.appendChild(createQuitBtn);
@@ -99,7 +92,7 @@ var quitBtn = function() {
 // create next button (progress to next page)
 var nextBtn = function() {
     var createNextBtn = document.createElement('button');
-    createNextBtn.textContent = "Next"
+    createNextBtn.textContent = "Next";
     createNextBtn.id = "next-btn";
     navBtnDivEl = document.getElementById("nav-btn-div");
     createNextBtn.addEventListener("click", function(){
@@ -113,14 +106,6 @@ var nextBtn = function() {
         }
     })
     navBtnDivEl.appendChild(createNextBtn);
-}
-
-// Populates the current quiz question
-var showQuestion = function() {
-    // check currentQuestion value, populate corresponding questionArr item
-    question = questionArr[currentQuestion].question
-    // mainTopTitle.textContent = ''
-    mainTopTitle.textContent = questionArr[currentQuestion].question
 }
 
 // Generate Answer Buttons for the Current Question
@@ -138,10 +123,10 @@ var generateButtons = function () {
         // create button element
         var createButton = document.createElement('button');
         // Add corresponding answer option as button text
-        createButton.textContent = questionArr[currentQuestion][i];
-        // Set buttonID to be the corresponding answer option used to verify answer
-        createButton.setAttribute("buttonId", i);
-        createButton.className = "question-btn";
+            createButton.textContent = questionArr[currentQuestion][i];
+            // Set buttonID to be the corresponding answer option used to verify answer
+            createButton.setAttribute("buttonId", i);
+            createButton.className = "question-btn";
         // Add button to the buttonHolder div created above
         buttonHolderEl.appendChild(createButton);
     }
@@ -150,21 +135,41 @@ var generateButtons = function () {
 // To be used upon clicking the Next button in the quiz, it will remove the previous generated answer buttons
 var removeButtons = function () {
     var selectBtnDiv = document.querySelector(".button-holder");
-        if (!!selectBtnDiv) {
-            selectBtnDiv.remove();
-        }
+    if (!!selectBtnDiv) {
+        selectBtnDiv.remove();
+    }
     var nextBtnEl = document.querySelector("#next-btn");
-        // Checks in next button exists
-        if (!!nextBtnEl) {
-            nextBtnEl.remove();
-        }
+    // Checks in next button exists
+    if (!!nextBtnEl) {
+        nextBtnEl.remove();
+    }
+    if (!!startBtnEl) {
+        startBtnEl.remove();
+    }
 };
-    
+
+var removeQuit = function() {
+    rmvQuit = document.getElementById("quit-btn")
+    if(!!rmvQuit) {
+        rmvQuit.remove();
+    }
+}
+
+// Populates the current quiz question
+var showQuestion = function() {
+    // check currentQuestion value, populate corresponding questionArr item
+    question = questionArr[currentQuestion].question;
+    // mainTopTitle.textContent = ''
+    mainTopTitle.textContent = questionArr[currentQuestion].question;
+}
+
+// Using global addEventListener, if event.target matches specified class get attribute for pushed button.
 var questionPressHandler = function(event) {
     var targetEl = event.target;
 
     if(targetEl.matches(".question-btn")) {
         var userChoice = targetEl.getAttribute("buttonId");
+        // After a choice has been made, disable all choice buttons
         document.querySelectorAll("[class=question-btn]").forEach((function(disable){disable.setAttribute("disabled","true");}));
         quizLogic(userChoice);
     }
@@ -177,8 +182,7 @@ var quizLogic = function(userChoice) {
     } else if (parseInt(userChoice) === questionArr[currentQuestion].answer && currentQuestion === questionArr.length - 1){
         // clearInterval(quizTimer)
         currentQuestion++
-        console.log("Correct!")
-        alert("Correct!");
+        console.log("Correct!");
         scoreAnswers++;
         endQuiz();
         // If user answered question incorrectly && it was the last question of the quiz, end quiz
@@ -186,27 +190,110 @@ var quizLogic = function(userChoice) {
         // clearInterval(quizTimer)
         currentQuestion++
         console.log("Wrong!")
-        alert("Wrong!");
         scoreTime-=5;
         endQuiz();
     // If user answered question correctly && it wasn't the last question of the quiz add points, generate next question button
     } else if(parseInt(userChoice) === questionArr[currentQuestion].answer) {
         console.log("Correct!")
-        alert("Correct!");
         scoreAnswers++;
         nextBtn();
     // If user answered question incorrectly && it wasn't the last question of the quiz subtract time, generate next question button
     } else if (parseInt(userChoice) !== questionArr[currentQuestion].answer) {
         console.log("Wrong!")
-        alert("Wrong!");
         scoreTime-=5;
         nextBtn();
     }
 }
 
+//  Create form for entering initials to save with highscore
+var createForm = function(){
+    // Generate div for the form
+    var formDiv = document.createElement("div");
+        formDiv.setAttribute("id", "form-handler");
+    // Create form element
+    var initialsForm = document.createElement("form");
+    // Create input for form
+    var initialsInput = document.createElement("input");
+        initialsInput.type = "text";
+        initialsInput.name = "initials-input";
+        initialsInput.id = "initials";
+        initialsInput.placeholder = "Enter Initials Here";
+    // Create form submit button for input
+    var initialsSubmit = document.createElement("input");
+        initialsSubmit.type = "submit";
+        initialsSubmit.setAttribute("value", "Save Highscore");
+        initialsSubmit.id = "sub-score";
+    // Append the created elements above
+    initialsForm.appendChild(initialsInput);
+    initialsForm.appendChild(initialsSubmit);
+    formDiv.appendChild(initialsForm);
+    mainCenterEl.appendChild(formDiv);
+}
+
+var initialSubmitHandler = function (event) {
+    event.preventDefault();
+    var userInitials = document.querySelector("#initials")
+        .value
+        .toUpperCase();
+    console.log(userInitials);
+    if (userInitials > 3 || userInitials < 2) {
+        alert("Please provide your initials");
+    } else {
+        highscoreObj = {
+            initials: userInitials,
+            scoreTime,
+            scoreAnswers,
+        }
+        console.log("Highscore Obj",highscoreObj);
+        updateHighscores();
+        showHighscores();
+    }
+
+};
+
+var loadHighscore = function(){
+    highscores = localStorage.getItem("highscores");
+    if(!highscores) {
+        highscores = [];
+    } else {
+        highscores = JSON.parse(highscores);
+    }
+}
+
+var updateHighscores = function() {
+    loadHighscore();
+    highscores.push(highscoreObj);
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+}
+//     console.log("Highscores", highscores)
+// }
+
+var showHighscores = function() {
+    removeButtons();
+    removeQuit();
+    loadHighscore();
+    highscores.sort(function(a, b) {
+        console.log("Sorted Array", a.scoreTime - b.scoreTime)
+    });
+    mainTopTitle.textContent = "Brain Picker Highscores";
+    mainCenterEl.innerHTML = "";
+    timerDivEl.className = "hidden-2";
+    viewHighscoresEl.className = "hidden-2";
+    var returnToQuiz = document.createElement("button");
+        returnToQuiz.class = "btn-style";
+        returnToQuiz.textContent = "Return to Quiz";
+    mainBottomEl.appendChild(returnToQuiz);
+    returnToQuiz.addEventListener("click", function(){
+        window.location.reload();
+        returnToQuiz.remove();
+    })
+    console.log("Highscores", highscores);
+}
+
 var startQuiz = function() {
     // Clean quiz parent containers
-    quizPrep();
+    flavorTextEl.className = "hidden";
+    removeButtons();
     // Gen nav container
     navBtnDiv();
     // Show quit button
@@ -219,69 +306,18 @@ var startQuiz = function() {
     quizLogic();
 }
 
-var createForm = function(){
-    var formDiv = document.createElement("div");
-        formDiv.setAttribute("id", "form-handler");
-    var initialsForm = document.createElement("form");
-    var initialsInput = document.createElement("input");
-        initialsInput.type = "text";
-        initialsInput.name = "initials-input";
-        initialsInput.id = "initials";
-        initialsInput.placeholder = "Enter Initials Here";
-    var initialsSubmit = document.createElement("input");
-        initialsSubmit.type = "submit";
-        initialsSubmit.setAttribute("value", "Save Highscore");
-        initialsSubmit.id = "sub-score";
-    initialsForm.appendChild(initialsInput);
-    initialsForm.appendChild(initialsSubmit);
-    formDiv.appendChild(initialsForm);
-    mainCenterEl.appendChild(formDiv);
-}
-
-var saveHighscores = function(event){
-    event.preventDefault();
-    console.log(event)
-}
-
-var initialSubmitHandler = function (event) {
-    event.preventDefault();
-    var userInitials = document.querySelector("#initials")
-        .value
-        .toUpperCase();
-    console.log(userInitials);
-};
-
 var endQuiz = function() {
     finalTime = scoreTime; 
     clearInterval(quizTimer);
     console.log(finalTime);
     removeButtons();
-    var removeQuit = document.getElementById("quit-btn")
-        if(!!removeQuit) {
-            removeQuit.remove();
-        }
+    removeQuit();
     mainTopTitle.textContent = "Quiz Finished";
     flavorTextEl.className = "show";
     flavorTextEl.textContent = "You have completed the Brain Picker Quiz with a final time of " + scoreTime + " seconds. You correctly answered " + scoreAnswers + " out of " + questionArr.length + " questions available. Use the form below to save your highscore!";
     createForm();
-    // var formDiv = document.createElement("div");
-    //     formDiv.setAttribute("id", "form-handler");
-    // var initialsForm = document.createElement("form");
-    // var initialsInput = document.createElement("input");
-    //     initialsInput.type = "text";
-    //     initialsInput.name = "initials-input";
-    //     initialsInput.id = "initials";
-    //     initialsInput.placeholder = "Enter Initials Here";
-    // var initialsSubmit = document.createElement("input");
-    //     initialsSubmit.type = "submit";
-    //     initialsSubmit.setAttribute("value", "Save Highscore");
-    //     initialsSubmit.id = "sub-score";
-    // initialsForm.appendChild(initialsInput);
-    // initialsForm.appendChild(initialsSubmit);
-    // formDiv.appendChild(initialsForm);
-    // mainCenterEl.appendChild(formDiv);    
 }              
-                
+
 startBtnEl.addEventListener("click",function(){
     console.log("click");
     // Assign quizTimer to setInterval, begin quizTimer
@@ -304,3 +340,4 @@ startBtnEl.addEventListener("click",function(){
 
 mainCenterEl.addEventListener("click", questionPressHandler);
 mainCenterEl.addEventListener("submit", initialSubmitHandler);
+viewHighscoresEl.addEventListener("click", showHighscores);
